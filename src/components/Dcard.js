9360f59b4921afa4d20deccb176a64825971dd6c
madhumithaa <madhumithaa1411@gmail.com>
2020-09-16 14:01:59 +0530

    ui has to be done

--- src/components/Dcard.js
+++ src/components/Dcard.js
@@ -3,15 +3,14 @@ import { Card } from "react-bootstrap";
 export default function Dcard(props) {
   let { project } = props;
   return (
-    <Card className="card-component shadow p-3 mb-4 bg-white rounded">
+    <Card className="card-component shadow p-3 mb-4 bg-white rounded text-capitalize">
       <Card.Body>
         <Card.Title>{project.name}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">
           {project.type}
         </Card.Subtitle>
+        No.of outlets
         <Card.Text>{project.outlets_count}</Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
       </Card.Body>
     </Card>
   );
