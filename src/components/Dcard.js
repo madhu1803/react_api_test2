afe76ae7b553ccbfca6926ff28adb944154e649e
madhumithaa <madhumithaa1411@gmail.com>
2020-09-16 14:12:36 +0530

    done

--- src/components/Dcard.js
+++ src/components/Dcard.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { IoLogoXbox } from "react-icons/io";
 export default function Dcard(props) {
   let { project } = props;
   return (
-    <Card className="card-component shadow p-3 mb-4 bg-white rounded text-capitalize">
+    <Card className="card-component shadow p-1 mb-4 bg-white rounded text-capitalize">
       <Card.Body>
+        <IoLogoXbox className="mb-5" style={{ fontSize: "30px" }} />
         <Card.Title>{project.name}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">
+        <Card.Subtitle className="mb-2 text-primary">
           {project.type}
         </Card.Subtitle>
-        No.of outlets
-        <Card.Text>{project.outlets_count}</Card.Text>
+        <hr />
+        <span className="text-muted">No.of outlets</span>
+        <Card.Text>
+          <strong>{project.outlets_count}</strong>
+        </Card.Text>
       </Card.Body>
     </Card>
   );
